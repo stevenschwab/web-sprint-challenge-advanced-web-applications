1be3a84ce46a0cf3a04855975e823ccb0a3712bc
Steve Schwab <schwabs@umich.edu>
2024-04-17 16:48:32 -0700

    if no token exists, render a navigate to login screen

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -5,9 +5,10 @@ import PT from 'prop-types'
 export default function Articles(props) {
   const { articles, getArticles, deleteArticle, setCurrentArticleId, currentArticleId } = props;
 
-  // ✨ implement conditional logic: if no token exists
-  // we should render a Navigate to login screen (React Router v.6)
-  
+  const token = localStorage.getItem('token')
+  if (!token) {
+    return <Navigate to="/" replace />;
+  }
 
   useEffect(() => {
     // ✨ grab the articles here, on first render only
