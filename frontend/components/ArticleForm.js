1c82e2a98372a0bdd04856a41401a1e2d8111d7e
Steve Schwab <schwabs@umich.edu>
2024-04-18 10:03:59 -0700

    destructure props

--- frontend/components/ArticleForm.js
+++ frontend/components/ArticleForm.js
@@ -5,7 +5,7 @@ const initialFormValues = { title: '', text: '', topic: '' }
 
 export default function ArticleForm(props) {
   const [values, setValues] = useState(initialFormValues)
-  // ✨ where are my props? Destructure them here
+  const { postArticle, updateArticle, setCurrentArticleId } = props;
 
   useEffect(() => {
     // ✨ implement
