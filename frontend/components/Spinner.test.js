8c17a2e6e24de40b1bb944e66e8c5751a691c945
Steve Schwab <schwabs@umich.edu>
2024-04-18 15:15:11 -0700

    import react, import render, import jest-dom library, import spinner, render spinner with false and true, and test if the spinner id is found or not

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,16 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
+import React from "react";
+import { render } from "@testing-library/react";
+import '@testing-library/jest-dom'
+import Spinner from "./Spinner"
+
+test('renders correctly with on prop', () => {
+  const { rerender } = render(<Spinner on={false} />);
+
+  expect(document.querySelector('#spinner')).not.toBeInTheDocument()
+
+  rerender(<Spinner on={true} />)
+
+  expect(document.querySelector('#spinner')).toBeInTheDocument();
 })
