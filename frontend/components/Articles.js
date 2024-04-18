160d7d7a07150c4d46d442afb9e7108140a39d38
Steve Schwab <schwabs@umich.edu>
2024-04-18 12:31:01 -0700

    complete articles component

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -29,8 +29,8 @@ export default function Articles(props) {
                   <p>Topic: {art.topic}</p>
                 </div>
                 <div>
-                  <button disabled={true} onClick={Function.prototype}>Edit</button>
-                  <button disabled={true} onClick={() => deleteArticle(currentArticleId)}>Delete</button>
+                  <button disabled={!!currentArticleId} onClick={() => setCurrentArticleId(art.article_id)}>Edit</button>
+                  <button disabled={!!currentArticleId} onClick={() => deleteArticle(art.article_id)}>Delete</button>
                 </div>
               </div>
             )
