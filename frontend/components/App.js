36e03fd49d827043e70338b55da287646135ffbf
Steve Schwab <schwabs@umich.edu>
2024-04-17 17:19:22 -0700

    pass spinnerOn to the spinner component and alias it as on

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -71,9 +71,9 @@ export default function App() {
   }
 
   return (
-    // ✨ fix the JSX: `Spinner`, `ArticleForm` and `Articles` expect props ❗
+    // ✨ fix the JSX: `Spinner`
     <>
-      <Spinner />
+      <Spinner on={spinnerOn} />
       <Message message={message} />
       <button id="logout" onClick={logout}>Logout from app</button>
       <div id="wrapper" style={{ opacity: spinnerOn ? "0.25" : "1" }}> {/* <-- do not change this line */}
